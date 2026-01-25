466512a65af3e11aeb8843543657a821ffe0d232
Nicholas M <memecake788@gmail.com>
2026-01-25 11:47:03 -0600

    Mor UI Tweaks

--- Scripts/GunData.gd
+++ Scripts/GunData.gd
@@ -1,5 +1,9 @@
 class_name GunData
 extends Resource
 
+@export_group("AMMO")
 @export var ammo_max : int
 @export var ammo_current : int
+
+@export_group("UI")
+@export var ui_position : PackedScene
--- Scripts/MessageBus.gd
+++ Scripts/MessageBus.gd
@@ -1,4 +1,4 @@
 extends Node
 
 signal update_ammo_ui(new_ammo_value: int)
-signal selected_slot_changed(slot_index: int, item_node)
+signal selected_slot_changed(slot_index: int, item_data: GunData)
